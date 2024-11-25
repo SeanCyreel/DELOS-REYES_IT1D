48d4e97e783991ac8471bd7b2b67b24d608deb92
SeanCyreel <dseancyreel@gmail.com>
2024-11-25 20:21:18 +0800

    Add files via upload

---
+++ log in with attempts.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+	int attemp = 0;
+	
+	while (attemp > 5); {
+		System.out.println("Enter your username:" );
+		String username  = scanner.nextLine();
+		System.out.println("Enter your password:" );
+		String password = scanner.nextLine();      String status 
+	}
+	}
+	public static String LoginValidator(String username, String password) {
+		
+	}
+}
